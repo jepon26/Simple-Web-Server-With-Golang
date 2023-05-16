543dead9b416c4daaca302f2975a768ff9798727
Elvin Javier Rueda <elvinrueda@yahoo.es>
2023-05-15 21:15:05 -0600

    final commit

--- main.go
+++ main.go
@@ -7,43 +7,43 @@ import (
 )
 
 
-func formHandler(w http.ResponseWriter, r *http.Request){
+func formHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}	
-	fmt.Fprintf(w, "POST request succesful")
+	fmt.Fprintf(w, "POST request successful")
 	name := r.FormValue("name")
-	address := r.FormValue("adress")
+	address := r.FormValue("address")
 	fmt.Fprintf(w, "Name = %s\n", name)
 	fmt.Fprintf(w, "Address = %s\n", address)
 }
 
 
 
-func helloHandler(w http.ResponseWriter, r *http.Request){
-	if r.URL.Path != "/hello"{
-		http.Error(w, "404 page not found", http.StatusNotFound)
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
 		return
 	}
-	if r.Method != "GET"{
-		http.Error(w, "Method is not supported", http.StatusNotFound)
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
 		return
 	}
-	fmt.Fprintf(w, "hello")
+	fmt.Fprintf(w, "hello!")
 }
 
 
 
 
-func main(){
-	fileServer := http.FileServer(http.Dir("./base"))
+func main() {
+	fileServer := http.FileServer(http.Dir("./src"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err !=nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
